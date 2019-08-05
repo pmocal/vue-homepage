1aab80e8d973a5d2d34af2791ccd15c70e33b599
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-08-05 15:20:11 -0400

    made subdirectory not a repo

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<ul>
+			<li v-on:click="selectTab('Home')"><a>Home</a></li>
+			<li v-on:click="selectTab('Examples')"><a>Examples</a></li>
+			<li v-on:click="selectTab('Contact')"><a>Contact</a></li>
+		</ul>
+		<tab-content :selection="selection"></tab-content>
+	</div>
+</template>
+
+<script>
+import TabContent from './TabContent.vue'
+
+export default {
+	name: "MenuBar",
+	data() {
+		return {
+			selection: 'Home',
+		}
+	},
+	methods: {
+		selectTab(string) {
+			this.selection = string;
+		}
+	},
+	components: {
+		TabContent
+	}
+}
+</script>
+
+<style>
+	ul {
+		display: flex;
+		justify-content: space-around;
+	}
+	li {
+		flex-direction: column;
+	}
+</style>
---
+++ src/components/TabContent.vue
@@ -0,0 +1,29 @@
+<template>
+	<div v-if="selection == 'Contact'">
+		<h2>Find us at:</h2>
+		<p>336 S 4th St, Brooklyn, NY 11211</p>
+	</div>
+	<div v-else-if="selection == 'Examples'">
+		<img src="../assets/baldzain.png">
+		<img src="../assets/shortzain.jpg">
+		<img src="../assets/bruiser.jpg">
+	</div>
+	<div v-else-if="selection == 'Home'">
+		<h1>Welcome to</h1>
+		<img src="../assets/j-lbarber.png">
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "TabContent",
+		props: ['selection']
+	}
+</script>
+
+<style>
+	img {
+		width: 20%;
+		height: 20%;
+	}
+</style>
