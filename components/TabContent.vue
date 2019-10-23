0d58aa49be4cdbeb07111d47c107edac9582ed67
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-10-23 12:55:47 -0400

    added Resume

--- components/TabContent.vue
+++ components/TabContent.vue
@@ -6,6 +6,8 @@
 	<div v-else-if="selection == 'Work'">
 		<h1>Portfolio</h1>
 		<a href="https://www.github.com/pmocal">My Github</a>
+		<h1>Resume</h1>
+		<a href="./assets/ParthivMohanResumeWithoutAddress.pdf">Link</a>
 	</div>
 	<div v-else-if="selection == 'Home'">
 		<h1>Welcome to Parthiv Mohan's website</h1>
